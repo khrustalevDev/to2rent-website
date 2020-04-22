145065e39667bede1ef41e59f882e01b4c030397
Err0rmaker <i.khrustalyov@mail.ru>
2020-04-23 00:56:47 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,6 @@
+$(function() {
+    $('.slider').slick({
+        prevArrow: '<button class="slick-arrow slick-prev"><img src="images/arrow_L.svg" alt="left"></button>',
+        nextArrow: '<button class="slick-arrow slick-next"><img src="images/arrow_R.svg" alt="right"></button>'
+    });
+});
